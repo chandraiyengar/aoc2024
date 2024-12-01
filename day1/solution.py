11a76f2d3dab6500c927fd90152d7a28da9c51e1
Chandrasekhar Iyengar <chandrasekhariyengar@gmail.com>
2024-12-01 10:47:59 +0000

    first solution

--- day1/solution.py
+++ day1/solution.py
@@ -1,1 +1,15 @@
-print("sol")
+f = open('input.txt', 'r')
+leftList = []
+rightList = []
+
+for line in f:
+  x = line.split()
+  leftList.append(x[0])
+  rightList.append(x[1])
+total = 0
+rightList.sort()
+leftList.sort()
+for x in range(len(leftList)):
+  total += abs(int(leftList[x]) - int(rightList[x]))
+
+print(total)
