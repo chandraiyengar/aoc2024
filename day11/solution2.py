58bf8e4e8525acd87edc3c6682c68c59440bbc45
Chandrasekhar Iyengar <chandrasekhariyengar@gmail.com>
2024-12-19 09:25:33 +0000

    day11/solution 2: don't cast to string

--- day11/solution2.py
+++ day11/solution2.py
@@ -1,4 +1,5 @@
 from time import perf_counter
+import math
 stones = [int(num) for num in open('input.txt').read().rstrip().split()]
 
 for step in range(75):
@@ -6,9 +7,9 @@ for step in range(75):
   new_stones = []
   for engraving in stones:
     if engraving == 0: new_stones.append(1)
-    elif (engraving_length := len(engraving_string := str(engraving))) % 2 == 0:
-      left = int(engraving_string[:engraving_length // 2]) # // (10 ** (len(str(engraving)) / 2))
-      right = int(engraving_string[engraving_length // 2:])
+    elif (engraving_length := math.floor(math.log10(engraving)) + 1) % 2 == 0:
+      left = engraving // (10 ** (engraving_length / 2))
+      right = engraving - (left * (10 ** (engraving_length / 2)))
       new_stones.append(left)
       new_stones.append(right)
     else:
